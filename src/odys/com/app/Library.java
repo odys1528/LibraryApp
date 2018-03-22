a997f955afd37c150ec0187a9ca7abd1a65ad221
odys1528 <jolanta.zakrzewska@orange.pl>
2018-03-22 17:24:03 +0100

    1D arrays

--- src/odys/com/app/Library.java
+++ src/odys/com/app/Library.java
@@ -5,17 +5,19 @@ import odys.com.data.Book;
 public class Library {
 
     public static void main(String[] args) {
-	    final String appName = "Biblioteka v.0.4";
+	    final String appName = "Biblioteka v.0.5";
 
-	    Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewcz", 2010, 296, "Greg", "9788373271890");
-		Book book2 = new Book("Java. Efektywne programowanie", "Joshua Bloch", 2009, 352, "Helion", "9780071591065");
-		Book book3 = new Book("SCJP Sun Certified Programmer for Java 8", "Bert Bates, Katherine Sierra", 2008, 851,
+	    Book[] books = new Book[1000];
+	    books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewcz", 2010, 296, "Greg", "9788373271890");
+		books[1] = new Book("Java. Efektywne programowanie", "Joshua Bloch", 2009, 352, "Helion", "9780071591065");
+		books[2] = new Book("SCJP Sun Certified Programmer for Java 8", "Bert Bates, Katherine Sierra", 2008, 851,
 				"McGraw-Hill Osborne Media", "9780071591065");
 
 	    System.out.println(appName);
 	    System.out.println("Książki dostępne w bibliotece:");
-		book1.printInfo();
-		book2.printInfo();
-		book3.printInfo();
+		books[0].printInfo();
+		books[1].printInfo();
+		books[2].printInfo();
+		System.out.println("System może przechowywać do 1000 książek.");
     }
 }
