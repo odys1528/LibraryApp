5ee0fe0d4445a31d4e9bbf82507f499d613d9fe3
odys1528 <jolanta.zakrzewska@orange.pl>
2018-03-22 16:47:26 +0100

    Initial commit

---
+++ src/odys/com/Library.java
@@ -0,0 +1,23 @@
+package odys.com;
+
+public class Library {
+
+    public static void main(String[] args) {
+	    final String appName = "Biblioteka v.0.1";
+	    String title = "W pustyni i w puszczy";
+	    String author = "Henryk Sienkiewcz";
+	    int releaseDate = 2010;
+	    int pages = 296;
+	    String publisher = "Greg";
+	    String isbn = "9788373271890";
+
+	    System.out.println(appName);
+	    System.out.println("Książki dostępne w bibliotece:");
+	    System.out.println(title);
+	    System.out.println(author);
+	    System.out.println(releaseDate);
+	    System.out.println(pages);
+	    System.out.println(publisher);
+	    System.out.println(isbn);
+    }
+}
