46c462254bb7f26dff969fc46e35ebd50667d859
odys1528 <jolanta.zakrzewska@orange.pl>
2018-03-22 19:26:41 +0100

    inner classes

--- src/odys/com/app/LibraryControl.java
+++ src/odys/com/app/LibraryControl.java
@@ -9,7 +9,6 @@ import odys.com.utils.LibraryUtils;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 
-import static odys.com.app.Option.*;
 
 public class LibraryControl {
     private DataReader dataReader;
@@ -75,4 +74,44 @@ public class LibraryControl {
     private void printMagazines() {
         LibraryUtils.printMagazines(library);
     }
+
+    public enum Option {
+        EXIT(0, "Wyjście z programu"),
+        ADD_BOOK(1, "Dodanie książki"),
+        ADD_MAGAZINE(2, "Dodanie magazynu"),
+        PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
+        PRINT_MAGAZINES(4, "Wyświetlenie dostępnych magazynów");
+
+        private int value;
+        private String description;
+
+        public int getValue() {
+            return value;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        Option(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        public static Option createFromInt(int option) {
+            Option result = null;
+            try {
+                result = Option.values()[option];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new NoSuchElementException("Nie odnaleziono elementu.");
+            }
+            return result;
+        }
+    }
+
 }
