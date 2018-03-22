74c53b9456b0da11d5eb5337ed98d8c0f67e49d8
odys1528 <jolanta.zakrzewska@orange.pl>
2018-03-22 17:07:10 +0100

    Book constructor & print method

--- src/odys/com/Book.java
+++ src/odys/com/Book.java
@@ -7,4 +7,19 @@ public class Book {
     int pages;
     String publisher;
     String isbn;
+
+    Book(String bookTitle, String bookAuthor, int bookReleaseDate, int bookPages, String bookPublisher,
+         String bookIsbn) {
+        title = bookTitle;
+        author = bookAuthor;
+        releaseDate = bookReleaseDate;
+        pages = bookPages;
+        publisher = bookPublisher;
+        isbn = bookIsbn;
+    }
+
+    void printInfo() {
+        String info = title + "; " + author + "; " + releaseDate + "; " + pages  + "; " + publisher + "; " + isbn;
+        System.out.println(info);
+    }
 }
