b9331c6fd9aff0f693e4b889424b794c6d9520f5
odys1528 <jolanta.zakrzewska@orange.pl>
2018-03-22 17:15:46 +0100

    access modifiers

--- src/odys/com/app/Library.java
+++ src/odys/com/app/Library.java
@@ -1,9 +1,11 @@
-package odys.com;
+package odys.com.app;
+
+import odys.com.data.Book;
 
 public class Library {
 
     public static void main(String[] args) {
-	    final String appName = "Biblioteka v.0.3";
+	    final String appName = "Biblioteka v.0.4";
 
 	    Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewcz", 2010, 296, "Greg", "9788373271890");
 		Book book2 = new Book("Java. Efektywne programowanie", "Joshua Bloch", 2009, 352, "Helion", "9780071591065");
